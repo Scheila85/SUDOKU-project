291f381692a8266e99de7d3a73c85281e236eb2d
scheila <sheillapaes08@gmail.com>
2026-03-30 00:20:22 -0300

    feat: adiciona UserInterfaceMain.java

---
+++ src/br/com/dio/sudoku/UserInterfaceMain.java
@@ -0,0 +1,27 @@
+package br.com.dio.sudoku;
+
+import br.com.dio.sudoku.ui.costum.frame.MainFrame;
+import br.com.dio.sudoku.ui.costum.panel.MainPanel;
+import br.com.dio.sudoku.ui.costum.screen.MainScreen;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserInterfaceMain {
+
+    static void main(String[] args) {
+
+        final Map<String, Integer> gameConfig = new HashMap<>();
+
+        for (String position : args) {
+            String [] partes = position.split(",");
+            String key = partes[0] +","+ partes[1];
+            int value = Integer.parseInt(partes[2]);
+            gameConfig.put(key,value);
+        }
+
+        MainScreen mainScreen = new MainScreen(gameConfig);
+        mainScreen.buildMainScreen();
+    }
+}
